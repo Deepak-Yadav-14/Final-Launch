2003c33ce33a01a43f4bf203b421ece338800093
DhruvKS <140548388+Prider16@users.noreply.github.com>
2025-05-05 00:29:30 +0530

    Shooting enemy AI and petrolling
    Here is the shooting enemy Ai with petrol mechanism, it has some issues with linear petrol, but the loop petrol is working just fine

--- scripts/Enemy Scripts/shoot_enemy.gd
+++ scripts/Enemy Scripts/shoot_enemy.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
+class_name ShootingEnemy
 
 @onready var ray_cast: RayCast2D = $RayCast
 @onready var timer: Timer = $Timer
-@export var bullet: PackedScene
 @export var player: CharacterBody2D
 @export var health: int = 10
+@onready var player_rotation: float
 
 var player_entered = false
 
@@ -15,21 +16,28 @@ enum States{
 
 var current_state = States.PETROLLING
 
-func _physics_process(delta: float) -> void:
+const bullet = preload("res://scenes/Enemy Scenes/Enemy_bullet.tscn")
+
+func _ready() -> void:
+	player_rotation = rotation
+
+func custome_process(delta: float) -> void:
 	if player_entered:
-		var to_player = player.global_position - global_position
-		rotation = to_player.angle() - PI / 2
+		#var to_player = player.global_position - global_position
+		var target_angle = (player.global_position - global_position).angle() - PI / 2
+		rotation = lerp_angle(rotation, target_angle, 5 * delta)
 		aim(player)
 		check_player_collision(player)
-	else:
-		pass
+
+func _physics_process(delta: float) -> void:
+	if current_state == States.PETROLLING and rotation != player_rotation:
+		rotation = lerp_angle(rotation, player_rotation, 0.1)
+	custome_process(delta)
 
 func check_player_collision(player) -> void:
 	if ray_cast.get_collider() == player and timer.is_stopped():
 		timer.start()
 	elif ray_cast.get_collider() != player and not timer.is_stopped():
-		timer.stop()
-	elif ray_cast.get_collider() != player and timer.is_stopped():
 		timer.stop()
 
 func _on_visiblity_body_entered(body: Node2D) -> void:
@@ -41,6 +49,7 @@ func _on_visiblity_body_entered(body: Node2D) -> void:
 
 func _on_visiblity_body_exited(body: Node2D) -> void:
 	player_entered = false
+	current_state = States.PETROLLING
 	if not timer.is_stopped():
 		timer.stop()
 
@@ -56,9 +65,6 @@ func shoot() -> void:
 	var to_player = (player.global_position - global_position).normalized()
 	ammo.direction = to_player
 	get_tree().current_scene.add_child(ammo)
-
-func petrolling(delta):
-	pass
 
 func take_damage():
 	health -= 1
