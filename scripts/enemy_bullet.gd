6f46c495a1c078e2420a3e27b4653b297f2ecd63
DhruvKS <140548388+Prider16@users.noreply.github.com>
2025-05-04 08:03:43 +0530

    Basic enemy Shoot AI

--- scripts/enemy_bullet.gd
+++ scripts/enemy_bullet.gd
@@ -1,22 +1,21 @@
 extends Area2D
 
-@export var bullet_speed:float = 1000
+@export var bullet_speed:float = 1500
 @export var bullet_range:float = 1200
 
 var travelled_distance:float = 0;
-
+var direction = Vector2.RIGHT
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector2.RIGHT.rotated(rotation)
+	direction = direction.rotated(rotation)
 	position += direction * bullet_speed * delta
 	travelled_distance += bullet_speed * delta
 	
 	if travelled_distance > bullet_range:
 		queue_free()
-	
-	
-func _on_area_entered(body):
-	if body.is_in_group("destructible") or body.is_in_group("enemy"):
-		# Implement take damage function for each enemy or destructible object
-		body.take_damage()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print("player got hit")
 	queue_free() 
---
+++ scripts/enemy_test.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+@onready var ray_cast: RayCast2D = $RayCast
+@onready var timer: Timer = $Timer
+@export var bullet: PackedScene
+@export var player: CharacterBody2D
+@export var health: int = 10
+
+var player_entered = false
+
+enum States{
+	PETROLLING,
+	FIGHTING
+}
+
+var current_state = States.PETROLLING
+
+func _physics_process(delta: float) -> void:
+	if player_entered:
+		var to_player = player.global_position - global_position
+		rotation = to_player.angle() - PI / 2
+		aim(player)
+		check_player_collision(player)
+	else:
+		pass
+
+func check_player_collision(player) -> void:
+	if ray_cast.get_collider() == player and timer.is_stopped():
+		timer.start()
+	elif ray_cast.get_collider() != player and not timer.is_stopped():
+		timer.stop()
+
+func _on_visiblity_body_entered(body: Node2D) -> void:
+	if body == player:
+		player_entered = true
+	else:
+		pass
+
+func _on_visiblity_body_exited(body: Node2D) -> void:
+	player_entered = false
+	if not timer.is_stopped():
+		timer.stop()
+
+func aim(player) -> void:
+	ray_cast.target_position = to_local(player.position)
+
+func _on_timer_timeout() -> void:
+	shoot()
+
+func shoot() -> void:
+	var ammo = bullet.instantiate()
+	ammo.position = global_position
+	var to_player = (player.global_position - global_position).normalized()
+	ammo.direction = to_player
+	get_tree().current_scene.add_child(ammo)
+
+func petrolling(delta):
+	pass
+
+func take_damage():
+	health -= 1
+	if health <= 0:
+		print("Enemie Dies")
