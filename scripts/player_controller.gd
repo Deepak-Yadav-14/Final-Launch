6310e6334d50c97f9b6e816d3bee63963d9cf00f
Deepak-Yadav-14 <143217869+Deepak-Yadav-14@users.noreply.github.com>
2025-05-04 18:32:23 +0530

    updated player to add assasinate and hide option

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,15 +1,77 @@
 extends CharacterBody2D
 
-var collected_fuel_tank: int = 0
 
+
+@onready var assasinate_zone: Area2D = $AssasinateZone
+
+#@export var table_sprite: Sprite2D
 @export var speed: float = 400
 @export var health: float = 10
+@export var is_detected: bool = false
+
+var collected_fuel_tank: int = 0
+var is_hiding: bool = false
+var can_assasinate: bool = false
+var is_in_hide_zone: bool = true
 
 func _physics_process(_delta: float) -> void:
-	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	if is_detected:
+		handle_normal_movement()
+		return
+	
+	if is_hiding:
+		if Input.get_vector("move_left","move_right","move_up","move_down") != Vector2.ZERO:
+			unhide()
+			return
+		if Input.is_action_just_pressed("assasinate"):
+			check_for_assasination()
+		return
+	handle_normal_movement()
+		
+func handle_normal_movement():
+	var direction = Input.get_vector("moasve_left","move_right","move_up","move_down")
 	velocity = direction * speed
 	move_and_slide()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("hide") and is_in_hide_zone and not is_detected:
+		if not is_hiding:	
+			print("Hiding")
+			hide_under_table()
+		else:
+			unhide()
 
+func unhide():
+	is_hiding = false
+	#table_sprite.modulate.a = 1.0
+	print("Player is no longer hiding")
+
+func hide_under_table():
+	is_hiding = true
+	can_assasinate = true
+	#table_sprite.modulate.a = 0.4
+	velocity = Vector2.ZERO
+	print("Player is now hiding")
+
+func check_for_assasination():
+	if not can_assasinate and not is_hiding:
+		return
+	
+	var enemies = assasinate_zone.get_overlapping_bodies()
+	for enemy in enemies:
+		if enemy.is_in_group("enemy"):
+			perform_assasination(enemy)
+			break
+
+
+func perform_assasination(enemy: Node2D):
+	global_position = enemy.global_position
+	# add enemy assasinating animation here
+	enemy.queue_free()
+	unhide()
+	can_assasinate = false
+	print("Enemy Assasinated")
+	
 
 func take_damage(damage:float):
 	health -= damage
@@ -18,6 +80,11 @@ func take_damage(damage:float):
 		# play animation of death
 		print("YOU DIED")
 		get_tree().reload_current_scene()
+		
+func on_detected_by_enemy():
+	is_detected = true
+	unhide()
+	print("Player has been caught stealth disabled ")
 
 func add_fuel():
 	collected_fuel_tank += 1
