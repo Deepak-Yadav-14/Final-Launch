dab5c493ca2da4a676cdb273060f8d3d65a2fb1e
DhruvKS <140548388+Prider16@users.noreply.github.com>
2025-05-11 23:54:25 +0530

    gun things

--- scripts/fuel_pickup.gd
+++ scripts/fuel_pickup.gd
@@ -6,10 +6,10 @@ extends Area2D
 var time_span: float = 0
 
 func _process(delta: float) -> void:
-	time_span += delta
-	if time_span >= lifetime:
-		call_deferred("queue_free")
-		time_span=0;
+    time_span += delta
+    if time_span >= lifetime:
+        call_deferred("queue_free")
+        time_span=0;
 
 func _on_body_entered(body) -> void:
     if body.is_in_group("Player"):
