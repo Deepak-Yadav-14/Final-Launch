959cb0e042e2384f27d9f4ac6dd4c0a16a435d4c
DhruvKS <140548388+Prider16@users.noreply.github.com>
2025-05-11 22:15:17 +0530

    rocket and other changes

--- scripts/Level/lab_level.gd
+++ scripts/Level/lab_level.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+@onready var dialogbox: Control = %Dialogbox
+
+func _ready() -> void:
+    var dialog_lines: Array[String] = ["Whew... it's done. The gun’s finally working.", "Now... all that's left is to escape this godforsaken place.", "No more hiding. No more running. It's do or die."]
+    dialogbox.show_dialog(dialog_lines)
+
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
     if body.name == "Player":
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -23,124 +23,124 @@ var can_assasinate: bool = false
 var curr_speed: float  = speed
 
 func _ready() -> void:
-	pass
-	#melee_cooldown.wait_time = melee_cooldown_time
-	#melee_cooldown.one_shot  = true
-	#melee_cooldown.autostart  = false
-	#melee_cooldown.stop()
+    pass
+    #melee_cooldown.wait_time = melee_cooldown_time
+    #melee_cooldown.one_shot  = true
+    #melee_cooldown.autostart  = false
+    #melee_cooldown.stop()
 
 func _physics_process(_delta: float) -> void:
-	# Basic Movement Logic
-	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	var base_scale_x = abs($"Torso".scale.x)
-	if (direction.x < 0):
-		$"Torso".scale.x =  -base_scale_x
-	elif (direction.x > 0):
-		$"Torso".scale.x = base_scale_x
+    # Basic Movement Logic
+    var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+    var base_scale_x = abs($"Torso".scale.x)
+    if (direction.x < 0):
+        $"Torso".scale.x =  -base_scale_x
+    elif (direction.x > 0):
+        $"Torso".scale.x = base_scale_x
 
-	
-	velocity = direction * curr_speed
-	move_and_slide()
-	
-	_update_animation(direction)
-	# Basic Crouch Mechanic
-	if Input.is_action_pressed("crouch"):
-		if not is_crouching:
-			is_crouching = true
-			curr_speed = speed * crouch_speed_factor
-			set_collision_mask_value(2,false)
-	else:
-		# if player is under the table and exits it
-		if not is_in_hide_zone:
-			if not is_crouching:
-				set_collision_mask_value(2,true)
-			else:
-				is_crouching = false
-				curr_speed = speed
-			
-	if is_in_hide_zone:
-		%"Gun".visible = false
-		if Input.is_action_just_pressed("assasinate"):
-			print("Hello")
-			check_for_assasination()
-		return
-	else:
-		%"Gun".visible = true	
-	#if Input.is_action_just_pressed("melee_attack") and melee_cooldown.is_stopped():
-		#perform_melee_attack()
-		
+    
+    velocity = direction * curr_speed
+    move_and_slide()
+    
+    _update_animation(direction)
+    # Basic Crouch Mechanic
+    if Input.is_action_pressed("crouch"):
+        if not is_crouching:
+            is_crouching = true
+            curr_speed = speed * crouch_speed_factor
+            set_collision_mask_value(2,false)
+    else:
+        # if player is under the table and exits it
+        if not is_in_hide_zone:
+            if not is_crouching:
+                set_collision_mask_value(2,true)
+            else:
+                is_crouching = false
+                curr_speed = speed
+            
+    if is_in_hide_zone:
+        %"Gun".visible = false
+        if Input.is_action_just_pressed("assasinate"):
+            print("Hello")
+            check_for_assasination()
+        return
+    else:
+        %"Gun".visible = true	
+    #if Input.is_action_just_pressed("melee_attack") and melee_cooldown.is_stopped():
+        #perform_melee_attack()
+        
 func _update_animation(input_vec: Vector2) -> void:
-	if is_crouching:
-		if input_vec.length() > 0:
-			#animation_player.play("crouch_run")
-			pass
-		else:
-			#animation_player.play("crouch_idle")
-			pass
-	else:
-		if input_vec.length() > 0:
-			animation_player.play("player_run")
-		else:
-			animation_player.play("player_idle")
+    if is_crouching:
+        if input_vec.length() > 0:
+            #animation_player.play("crouch_run")
+            pass
+        else:
+            #animation_player.play("crouch_idle")
+            pass
+    else:
+        if input_vec.length() > 0:
+            animation_player.play("player_run")
+        else:
+            animation_player.play("player_idle")
 #func perform_melee_attack() -> void:
-	#is_attacking=true
-	#melee_cooldown.start()
-	## Optional: Add animation or visual feedback here
-	#
-	#print("Player performs melee attack")
-	#
-	#var bodies = melee_area.get_overlapping_bodies()
-	#var objects =  melee_area.get_overlapping_areas() 
-	#print(bodies)
-	#
-	#for body in bodies:
-		#if body.is_in_group("enemy") or body.is_in_group("destructible") :
-			#print("Enemy Detected")
-			#body.take_damage(100)
-	#for area in objects:
-		#if area.is_in_group("destructible") :
-			#print("Object Detected")
-			#area.take_damage(100)
-	#
-	#is_attacking = false
+    #is_attacking=true
+    #melee_cooldown.start()
+    ## Optional: Add animation or visual feedback here
+    #
+    #print("Player performs melee attack")
+    #
+    #var bodies = melee_area.get_overlapping_bodies()
+    #var objects =  melee_area.get_overlapping_areas() 
+    #print(bodies)
+    #
+    #for body in bodies:
+        #if body.is_in_group("enemy") or body.is_in_group("destructible") :
+            #print("Enemy Detected")
+            #body.take_damage(100)
+    #for area in objects:
+        #if area.is_in_group("destructible") :
+            #print("Object Detected")
+            #area.take_damage(100)
+    #
+    #is_attacking = false
 
 func check_for_assasination() -> void:
-	if not can_assasinate and not is_in_hide_zone:
-		return
-	
-	var enemies = assasinate_zone.get_overlapping_bodies()
-	print(enemies)
-	for enemy in enemies:
-		if enemy.is_in_group("enemy"):
-			perform_assasination(enemy)
-			break
+    if not can_assasinate and not is_in_hide_zone:
+        return
+    
+    var enemies = assasinate_zone.get_overlapping_bodies()
+    print(enemies)
+    for enemy in enemies:
+        if enemy.is_in_group("enemy"):
+            perform_assasination(enemy)
+            break
 
 
 func perform_assasination(enemy: Node2D) -> void:
-	global_position = enemy.global_position
-	# add enemy assasinating animation here
-	enemy.queue_free()
-	print("Enemy Assasinated")
-	
+    global_position = enemy.global_position
+    # add enemy assasinating animation here
+    enemy.queue_free()
+    print("Enemy Assasinated")
+    
 
 func take_damage(damage:float) -> void:
-	health -= damage
-	# play hurt animation
-	if health <= 0:
-		# play animation of death
-		print("YOU DIED")
-		get_tree().reload_current_scene()
-		
+    health -= damage
+    # play hurt animation
+    if health <= 0:
+        # play animation of death
+        print("YOU DIED")
+        get_tree().reload_current_scene()
+        
 func on_detected_by_enemy() -> void:
-	is_detected = true
-	print("Player has been caught stealth disabled ")
+    is_detected = true
+    print("Player has been caught stealth disabled ")
 
 func add_fuel() -> void:
-	collected_fuel_tank += 1
-	print("Collected Fuel : " , collected_fuel_tank)
+    collected_fuel_tank += 1
+    print("Collected Fuel : " , collected_fuel_tank)
 
 
 func _on_hurt_detector_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Hurt_Zone"):
-		take_damage(area.bullet_damage)
-		area.queue_free()
+    if area.is_in_group("Hurt_Zone"):
+        take_damage(area.bullet_damage)
+        area.queue_free()
